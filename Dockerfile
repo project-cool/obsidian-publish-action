53fa25a674887f6c1304e00343cad040be6118b8
Sarthak Narayan <sarthak.narayang@gmail.com>
2022-06-11 22:12:09 +0530

    checking for imagemagick installation

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ RUN chmod +x /app/entrypoint.sh
 
 RUN git clone https://github.com/project-cool/obsidian-to-mkdocs /app/code
 
+RUN apt update && apt -y install imagemagick
+
 ENTRYPOINT ["/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 echo "Cloning the docs repostiory: ${GITHUB_REPOSITORY}"
-git clone "https://github.com/${GITHUB_REPOSITORY}" /app/docs
-echo "`ls -alh /app/docs`"
-ls -alh /app/docs
+# git clone "https://github.com/${GITHUB_REPOSITORY}" /app/docs
+git clone "https://github.com/arkalim/obsidian-vault" /app/docs
+# ls -alh /app/docs
