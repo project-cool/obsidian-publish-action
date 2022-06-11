e7ac9f9bddac1202cf922f1fa4728b0926934ba9
Sarthak Narayan <sarthak.narayang@gmail.com>
2022-06-11 22:49:09 +0530

    test mkdocs

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ echo "Cloning the docs repostiory: ${GITHUB_REPOSITORY}"
 # git clone "https://github.com/${GITHUB_REPOSITORY}" /app/docs
 git clone "https://github.com/arkalim/obsidian-vault" /app/docs
 mkdocs new test
+python3 
 # ls -alh /app/docs
