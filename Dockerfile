3457e7b1769b939fa37e18801acec2b63badd2ea
Sarthak Narayan <sarthak.narayang@gmail.com>
2022-06-12 00:37:19 +0530

    test mkdocs

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16
+FROM nikolaik/python-nodejs:latest
 
 COPY entrypoint.sh /app/entrypoint.sh
 
@@ -6,7 +6,8 @@ RUN chmod +x /app/entrypoint.sh
 
 RUN git clone https://github.com/project-cool/obsidian-to-mkdocs /app/code
 
-# RUN apt update && apt -y install imagemagick
+RUN apt update && apt -y install imagemagick
+
 RUN pip3 install mkdocs-material
 
 ENTRYPOINT ["/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@
 set -e
 
 echo "Cloning the docs repostiory: ${GITHUB_REPOSITORY}"
-# git clone "https://github.com/${GITHUB_REPOSITORY}" /app/docs
-git clone "https://github.com/arkalim/obsidian-vault" /app/docs
+# git clone -b publish "https://github.com/${GITHUB_REPOSITORY}" /app/docs
+git clone -b publish "https://github.com/arkalim/obsidian-vault" /app/docs
 mkdocs new test
+ls -alh /app/docs
+ls -alh /app/code
 python3 
-# ls -alh /app/docs
