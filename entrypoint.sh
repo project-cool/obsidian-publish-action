07385d05d364254ef5a5d483893b1e550f727bc3
Sarthak Narayan <sarthak.narayang@gmail.com>
2022-06-12 01:12:26 +0530

    test mkdocs

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ npm run start "/app/docs" "${INPUT_LOG_LEVEL}"
 cd /app
 cp /app/docs/mkdocs.yml /app
 pwd
+chmod +x /app/code/scripts/compress.sh /app/code/scripts/build.sh /app/code/scripts/upload.sh
 /app/code/scripts/compress.sh "/app/docs"
 /app/code/scripts/build.sh "/app/docs"
 /app/code/scripts/upload.sh "/app/docs" "${INPUT_WEBSITEID}" "gtnoI3u0hGeUmOTISm8fV-FLRF0FVpmJLNbh5KevkEo"
