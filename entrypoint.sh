4add2774c94d251d909a4f7500a384b73121e7ee
Sarthak Narayan <sarthak.narayang@gmail.com>
2022-06-12 00:50:54 +0530

    test mkdocs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 echo "Cloning the docs repostiory: ${GITHUB_REPOSITORY}"
 # git clone -b publish "https://github.com/${GITHUB_REPOSITORY}" /app/docs
-git clone -b publish "https://github.com/arkalim/obsidian-vault" /app/vault
-cp /app/vault/mkdocs.yml /app
+git clone -b publish "https://github.com/arkalim/obsidian-vault" /app/docs
+cp /app/docs/mkdocs.yml /app
 cd /app
 mkdocs build
 ls -alh
