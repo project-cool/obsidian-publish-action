aab8614e36b204dd4a9c0593935b92a4605bfcdf
Sarthak Narayan <sarthak.narayang@gmail.com>
2022-06-12 22:10:01 +0530

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,20 @@
 
 set -e
 
-echo "Cloning the docs repostiory: ${GITHUB_REPOSITORY}"
+echo "[INFO] Cloning the docs repostiory: ${GITHUB_REPOSITORY}"
 git clone -b publish "https://github.com/${GITHUB_REPOSITORY}" /app/docs
-# git clone -b publish "https://github.com/arkalim/obsidian-vault" /app/docs
-cd /app/code 
+
+echo "${INPUT_LOG_LEVEL}"
+echo "${INPUT_WEBSITEID}"
+echo "${INPUT_BRANCH}"
+echo "${INPUT_IMAGE_RESOLUTION}"
+echo "${INPUT_COMPRESSION_FACTOR}"
+
+exit 1
+
+cd /app/code
 npm run start "/app/docs" "${INPUT_LOG_LEVEL}"
+
 cd /app
 cp /app/docs/mkdocs.yml /app
 chmod +x /app/code/scripts/compress.sh /app/code/scripts/build.sh /app/code/scripts/upload.sh
